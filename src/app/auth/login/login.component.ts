ee4ad13a76f64942c9a7015b8a27eb39c78ed42f
Luispolio1562000 <guspolio@outlook.es>
2024-09-25 13:06:02 -0600

    Se inicio el login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { NavComponent } from '../components/nav/nav.component';
+import { LogoComponent } from "../../shared/components/logo/logo.component";
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [NavComponent],
+  imports: [NavComponent, LogoComponent, InputTextModule, PasswordModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
