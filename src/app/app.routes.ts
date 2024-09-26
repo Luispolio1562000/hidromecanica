8286dc1e45f943ec160a0b23c8a9046ce726bf72
Diego <diego.espinoza@qrsof.com>
2024-09-26 08:44:14 -0600

    se actualizo el bg

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { authGuard } from './core/auth.guard';
+
 
 export const routes: Routes = [
   { path: '', loadChildren: () => import('./auth/auth.routes').then(m => m.authRoutes) },
